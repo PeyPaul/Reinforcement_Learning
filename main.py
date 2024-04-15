7ab457e77a7d12adc200b0d2e6060a7ef9494e49
Paul Peytevin <ppeytevin@gmail.com>
2024-04-15 22:43:50 +0200

    fix the issue with the depreceted version

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
 ### Setup the game
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = gym_super_mario_bros.make('SuperMarioBros-v0',apply_api_compatibility=True,render_mode="human" )
 env = JoypadSpace(env, SIMPLE_MOVEMENT) #to drop possible action from 256 to 7
+
+done = True
+for step in range(100000):
+    if done:
+        env.reset()
+    state, reward, done, truncated, info = env.step(env.action_space.sample())
+    env.render()
+env.close()
