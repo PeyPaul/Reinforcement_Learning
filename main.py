00fe1893c674c88b552d5b459b6da1f1b49857fc
Paul Peytevin <ppeytevin@gmail.com>
2024-04-15 22:17:03 +0200

    import dependencies

---
+++ main.py
@@ -0,0 +1,9 @@
+### IMPORT DEPENDENCIES
+
+# import the game
+import gym_super_mario_bros
+# import the Joypad wrapper
+from nes_py.wrappers import JoypadSpace
+# import the simplified controls
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
