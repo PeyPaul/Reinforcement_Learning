9e2d265fd86b202c641c71d8c2ecf16df3d8848a
Paul Peytevin <ppeytevin@gmail.com>
2024-04-17 10:39:29 +0200

    importationof RL stuff

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 env = gym_super_mario_bros.make('SuperMarioBros-v0',apply_api_compatibility=True,render_mode="human" )
 env = JoypadSpace(env, SIMPLE_MOVEMENT) #to drop possible action from 256 to 7
 
-done = True
-for step in range(100000):
-    if done:
-        env.reset()
-    state, reward, done, truncated, info = env.step(env.action_space.sample())
-    env.render()
-env.close()
+#done = True
+#for step in range(100000):
+#    if done:
+#        env.reset()
+#    state, reward, done, truncated, info = env.step(env.action_space.sample())
+#    env.render()
+#env.close()
+
+### Preprocessing environement
+
+# import Frame Stacker Wrapper and Grayscaling Wrapper
+from gym.wrappers import FrameStack, GrayScaleObservation
+# Import vectorization wrappers
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+# Import Matplotlib
+from matplotlib import pyplot as plt
