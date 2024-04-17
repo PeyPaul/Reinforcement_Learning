e87b1f9a39302aa3ddae99d010669673f35d8eb5
Paul Peytevin <ppeytevin@gmail.com>
2024-04-17 11:48:10 +0200

    Setting Up the Training Callback

--- main.py
+++ main.py
@@ -40,6 +40,32 @@ env = VecFrameStack(env, 4, channels_order = 'last')
 # import os for file path management
 import os
 # import PPO
-from stable_baselines3 import ppo
+from stable_baselines3 import PPO
 # import base Callback for saving models
 from stable_baselines3.common.callbacks import BaseCallback
+
+class TrainAndLoggingCallback(BaseCallback):
+    
+    def __init__(self, check_freq, save_path, verbose = 1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+        
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok = True)
+            
+    def _on_step(self):
+        if self.n_calls & self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+            
+        return True
+    
+CHECKPOINT_DIR = './train/'
+LOG_DIR = './logs/'
+
+# setup model saving callback
+callback = TrainAndLoggingCallback(check_freq = 100000, save_path = CHECKPOINT_DIR) #by increasing check_freq we reduce the amount of memoryused by the model
+
+model = PPO('CnnPolicy', env, verbose = 1, tensorboard_log = LOG_DIR, learning_rate = 0.000001, n_steps = 512)
